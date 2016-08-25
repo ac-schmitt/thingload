82d7a0b308f099cd9ee0a6276a964e2250f5ecb7
Alexander Schmitt <alexander.schmitt@web.de>
2016-08-25 14:43:50 +0200

    Created setup.py

--- README.rst
+++ README.rst
@@ -5,8 +5,32 @@ This is a sample project to push the CPU load of a windows machine to AWS IoT us
 
 It shall demonstrate the use of the device shadow.
 
-Planned Features
+It uses the AWSIoTPythonSDK https://github.com/aws/aws-iot-device-sdk-python.
+
+Features:
 
 - Fetch CPU load from Windows (done)
 - Publish the CPU load to an AWS IoT shadow (done)
-- The publish rate is a Shadow Attribute and can be changed (proposed) by others
+- The publish rate is a Shadow Attribute and can be changed (proposed) by others (planned)
+
+Installation
+~~~~~~~~~~~~
+
+Minimum Requirements
+____________________
+
+-  Python 3.5+
+-  Windows 7+ (because it calls windows commands to fetch the cpu load)
+-  OpenSSL version 1.0.1+ (TLS version 1.2) compiled with the Python executable for
+   X.509 certificate-based mutual authentication
+
+   To check your version of OpenSSL, use the following command in a Python interpreter:
+
+   .. code-block:: python
+
+       >>> import ssl
+       >>> ssl.OPENSSL_VERSION
+
+Usage
+~~~~~
+
--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
-# -*- coding: utf-8 -*-
+'''
+/*
+ * Copyright 2016 Alexander Schmitt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
 
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
-    name='sample',
-    version='0.0.1',
-    description='Sample package for Python-Guide.org',
+    name="thingload",
+    version="0.0.1",
+    description="Sample project to push the CPU load to AWS IoT",
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author="Alexander Schmitt",
+    author_email="alexander.al.schmitt@deutschebahn.com",
+    url="https://github.com/ac-schmitt/thingload",
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=("tests", "docs"))
 )
-
