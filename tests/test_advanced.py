42c322ec0634fb9b8bbfcee0c8e15baa6b6290fb
Alexander Schmitt <alexander.schmitt@web.de>
2016-08-25 08:54:08 +0200

    Finished reading cpu load function

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -7,9 +7,9 @@ import unittest
 
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
-
-    def test_thoughts(self):
-        thingload.hmm()
+    
+    def test_cpuload(self):
+        assert thingload.fetchCpuLoad() != "unknown"
 
 
 if __name__ == '__main__':
--- thingload/core.py
+++ thingload/core.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+import subprocess
+import re
 
-def hmm():
-    """Contemplation..."""
-    print get_hmm()
+def fetchCpuLoad():
+    wmic = subprocess.run(["wmic", "cpu", "get", "loadpercentage"], stdout=subprocess.PIPE, universal_newlines=False)
+    match = re.search("[0-9]+", wmic.stdout.decode("utf-8"))
+    if match:
+        return match.group(0)
+    else:
+        return "unknown"
+
